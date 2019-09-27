f5a118b72a1e30fa2d51a7d8aa7088c18697754d
DanLaZ <lazenberry_daniel@yahoo.com>
2019-09-27 10:06:54 -0400

    Heroku JAWS_db push

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
+DROP DATABASE IF EXISTS vzplnqmodk23vfmo;
+
+CREATE DATABASE vzplnqmodk23vfmo;
+
+USE vzplnqmodk23vfmo;
+
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(45) NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
